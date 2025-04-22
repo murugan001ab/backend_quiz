e78f3865433a3003967ac068349f4d2e45718472
murugan <murugan001ab@gmail.com>
2025-04-22 15:38:22 +0530

    setting

--- quiz/settings.py
+++ quiz/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS=["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
